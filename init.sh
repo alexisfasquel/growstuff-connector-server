6b50ec520b17922b4a80560efcc443b4c7de4039
alexisfasquel <alexis.fsq@gmail.com>
2014-03-30 16:07:57 +0000

    Fixing rc.local

--- init.sh
+++ init.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
 # Dowloading the init branch and unziping
-wget -N https://github.com/alexisfasquel/growstuff-connector-server/archive/init-script.zip
-rm -R growstuff-connector-server-init-script 2>/dev/null
-unzip init-script.zip
+rm -R /tmp/growstuff-connector-server-init-script 2>/dev/null
+wget https://github.com/alexisfasquel/growstuff-connector-server/archive/init-script.zip
+unzip -d /tmp init-script.zip
 rm init-script.zip 2>/dev/null
 
 # Extracting the res folder
-rm -R ./res
-mv -f growstuff-connector-server-init-script/res ./res
-rm -R growstuff-connector-server-init-script 2>/dev/null
+rm -R /tmp/res 2>/dev/null
+mv -f /tmp/growstuff-connector-server-init-script/res /tmp/res
+rm -R /tmp/growstuff-connector-server-init-script 2>/dev/null
 
 # Updating intefaces configurations
-mv -f ./res/interfaces /etc/network/interfaces
+mv -f /tmp/res/interfaces /etc/network/interfaces
 
 # Removing the automatic run of wpa_supplicant
 rm /usr/share/dbus-1/system-services/fi.w1.wpa_supplicant1.service 2>/dev/null
@@ -21,22 +21,26 @@ rm /usr/share/dbus-1/system-services/fi.w1.wpa_supplicant1.service 2>/dev/null
 apt-get install isc-dhcp-server
 
 # Changing the init script
-mv -f  ./res/isc-dhcp-server /etc/default/isc-dhcp-server
+mv -f  /tmp/res/isc-dhcp-server /etc/default/isc-dhcp-server
 
 # Changing the mac adress on the dhcp conf file
 address="`cat /sys/class/net/wlan0/address`"
-sed -i "s/xx:xx:xx:xx:xx/$address/" res/dhcpd.conf
+sed -i "s/xx:xx:xx:xx:xx/$address/" /tmp/res/dhcpd.conf
+
+# Changing the config
+mv -f /tmp/res/dhcpd.conf /etc/dhcp/dhcpd.conf
 
 # Preventing the dhcp server from stating when booting
 update-rc.d -f isc-dhcp-server remove
 
 # Launching the deamon at startup
-mv -f ./res/rc.local /etc/rc.local
+mv -f /tmp/res/rc.local /etc/rc.local
 
-# Getting the "server side" scripts and moving it 
-wget https://github.com/alexisfasquel/growstuff-connector-server/archive/master.zip
-unzip master.zip
+# Getting the "server side" scripts and moving it
+wget  https://github.com/alexisfasquel/growstuff-connector-server/archive/master.zip
+rm -R /tmp/growstuff-connector-server-master 2>/dev/null
+unzip -d /tmp  master.zip
 rm master.zip 2>/dev/null
-rm - R /home/pi/network 2>/dev/null
-mv -f growstuff-connector-server-master /home/pi/network
-rm -R res 2>/dev/null
+rm -R /home/pi/network 2>/dev/null
+mv -f /tmp/growstuff-connector-server-master /home/pi/network
+rm -R /tmp/res 2>/dev/null
