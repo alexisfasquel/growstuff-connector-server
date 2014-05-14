635e17b96046b17e1b09cf1767d8ab9672587f39
fasquelito <alexis.fasquel@insa-lyon.fr>
2014-05-15 00:04:00 +0200

    Cleaning up the mess

--- js/app.js
+++ js/app.js
@@ -1,1 +1,26 @@
+<<<<<<< HEAD
 $(document).foundation();
+
+
+$(document).ready(function() {
+    
+    $('#drop-button').click(function () {
+        var menu = $('#drop-menu');
+        if($('#drop-menu').is(':visible')) {
+            menu.hide();
+        } else {
+            menu.show();    
+        }
+        return false;
+    })
+});
+
+
+$(window).resize(function() {
+    if($(this).width() > 599) {
+        $('#drop-menu').hide();
+    }
+});
+=======
+$(document).foundation();
+>>>>>>> FETCH_HEAD
